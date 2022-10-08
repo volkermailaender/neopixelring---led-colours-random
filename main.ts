7e014397e7c3bbd2f77133cbc3a1955f7aef4cca
volkermailaender <volker.mailaender@gmx.de>
2022-10-08 12:45:48 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+let strip = neopixel.create(DigitalPin.P0, 12, NeoPixelMode.RGB)
+strip.setBrightness(20)
+strip.show()
 basic.forever(function () {
-	
+    for (let Index = 0; Index <= 12; Index++) {
+        strip.setPixelColor(Index, neopixel.rgb(randint(-100, 255), randint(-100, 255), randint(-100, 255)))
+    }
 })
